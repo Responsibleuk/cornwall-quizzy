59a3681a3318e52bd184522e7432648c8c9dc0a4
Responsibleuk <james@we-are-responsible.com>
2021-12-30 14:56:48 +0000

    add js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,138 +1,3 @@
-// ============= Nav bar =============
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
-
-const navToggle = document.querySelector(".nav-toggle");
-const links = document.querySelector(".links");
-
-navToggle.addEventListener("click", function () {
-  // console.log(links.classList);
-  // console.log(links.classList.contains("random"));
-  // console.log(links.classList.contains("links"));
-  // if (links.classList.contains("show-links")) {
-  //   links.classList.remove("show-links");
-  // } else {
-  //   links.classList.add("show-links");
-  // }
-  links.classList.toggle("show-links");
-});
-
-// ============= Countdown =============
-
-// const months = [
-//     'January',
-//     'February',
-//     'March',
-//     'April',
-//     'May',
-//     'June',
-//     'July',
-//     'August',
-//     'September',
-//     'October',
-//     'November',
-//     'December',
-//   ];
-//   const weekdays = [
-//     'Sunday',
-//     'Monday',
-//     'Tuesday',
-//     'Wednesday',
-//     'Thursday',
-//     'Friday',
-//     'Saturday',
-//   ];
-//   const giveaway = document.querySelector('.giveaway');
-//   const deadline = document.querySelector('.deadline');
-//   const items = document.querySelectorAll('.deadline-format h4');
-  
-//   let tempDate = new Date();
-//   let tempYear = tempDate.getFullYear();
-//   let tempMonth = tempDate.getMonth();
-//   let tempDay = tempDate.getDate();
-//   // months are ZERO index based;
-//   const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
-  
-//   // let futureDate = new Date(2020, 3, 24, 11, 30, 0);
-  
-//   const year = futureDate.getFullYear();
-//   const hours = futureDate.getHours();
-//   const minutes = futureDate.getMinutes();
-  
-//   let month = futureDate.getMonth();
-//   month = months[month];
-//   const weekday = weekdays[futureDate.getDay()];
-//   const date = futureDate.getDate();
-//   giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
-  
-//   const futureTime = futureDate.getTime();
-//   function getRemaindingTime() {
-//     const today = new Date().getTime();
-  
-//     const t = futureTime - today;
-//     // 1s = 1000ms
-//     // 1m = 60s
-//     // 1hr = 60m
-//     // 1d = 24hr
-//     // values in miliseconds
-//     const oneDay = 24 * 60 * 60 * 1000;
-//     const oneHour = 60 * 60 * 1000;
-//     const oneMinute = 60 * 1000;
-//     // calculate all values
-//     let days = t / oneDay;
-//     days = Math.floor(days);
-//     let hours = Math.floor((t % oneDay) / oneHour);
-//     let minutes = Math.floor((t % oneHour) / oneMinute);
-//     let seconds = Math.floor((t % oneMinute) / 1000);
-  
-//     // set values array
-//     const values = [days, hours, minutes, seconds];
-//     function format(item) {
-//       if (item < 10) {
-//         return (item = `0${item}`);
-//       }
-//       return item;
-//     }
-  
-//     items.forEach(function (item, index) {
-//       item.innerHTML = format(values[index]);
-//     });
-  
-//     if (t < 0) {
-//       clearInterval(countdown);
-//       deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
-//     }
-//   }
-//   // countdown;
-//   let countdown = setInterval(getRemaindingTime, 1000);
-//   //set initial values
-//   getRemaindingTime();
-
-// // ============= tabs =============
-
-// const about = document.querySelector(".about");
-// const btns = document.querySelectorAll(".tab-btn");
-// const articles = document.querySelectorAll(".content");
-// about.addEventListener("click", function (e) {
-//   const id = e.target.dataset.id;
-//   if (id) {
-//     // remove selected from other buttons
-//     btns.forEach(function (btn) {
-//       btn.classList.remove("active");
-//     });
-//     e.target.classList.add("active");
-//     // hide other articles
-//     articles.forEach(function (article) {
-//       article.classList.remove("active");
-//     });
-//     const element = document.getElementById(id);
-//     element.classList.add("active");
-//   }
-// });
-
 // ============= Quiz =============
 
 const question = document.querySelector('#question');
@@ -186,7 +51,7 @@ let questions = [
         choice2: "Ben Ainslie",
         choice3: "Helen Glover",
         choice4: "Philip Schofield",
-        answer: 1,
+        answer: 4,
     },
     {
         question: "The 1990s adaptation of Roald Dahl’s ‘The Witches’ was partly filmed in which Cornish hotel?",
@@ -305,9 +170,6 @@ const saveScoreBtn = document.querySelector('#saveScoreBtn')
 const finalScore = document.querySelector('#finalScore')
 const mostRecentScore = localStorage.getItem('mostRecentScore')
 
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
-
-const MAX_HIGH_SCORES = 5
 
 finalScore.innerText = mostRecentScore
 
