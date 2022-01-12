7636d970fe8deb69c71087ef73e1609bb010a162
Responsibleuk <james@we-are-responsible.com>
2022-01-12 15:59:35 +0000

    Add styling

--- assets/script/inspire.js
+++ assets/script/inspire.js
@@ -5,7 +5,7 @@ const reviews = [
     name: "borrito",
     job: "web developer",
     img:
-  "assets/images/1.jpg",
+      "assets/images/1.jpg",
     text:
       "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
@@ -14,25 +14,25 @@ const reviews = [
     name: "anna johnson",
     job: "web designer",
     img:
-  "assets/images/2.jpg",
-  text:
+      "assets/images/2.jpg",
+    text:
       "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
   },
   {
     id: 3,
-    name: "peter jones",
+    name: "curry",
     job: "intern",
     img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      "assets/images/3.jpg",
     text:
       "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
   },
   {
     id: 4,
-    name: "bill anderson",
+    name: "chilli",
     job: "the boss",
     img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      "assets/images/1.jpg",
     text:
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
