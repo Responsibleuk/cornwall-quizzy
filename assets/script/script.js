7b05af97c7a4b8b3d69a911b0220f9c978d4d86f
Responsibleuk <james@we-are-responsible.com>
2022-01-13 16:02:21 +0000

    Add script

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,11 +3,11 @@
 function tabulateAnswers() {
     // initialize variables for each choice's score
     // If you add more choices and outcomes, you must add another variable here.
-    var c1score = 0;
-    var c2score = 0;
-    var c3score = 0;
-    var c4score = 0;
-    var c5score = 0;
+    var a1score = 0;
+    var a2score = 0;
+    var a3score = 0;
+    var a4score = 0;
+    var a5score = 0;
     
     
     // get a list of the radio inputs on the page
@@ -17,20 +17,20 @@ function tabulateAnswers() {
       // if the radio is checked..
       if (choices[i].checked) {
         // add 1 to that choice's score
-        if (choices[i].value == 'c1') {
-          c1score = c1score + 1;
+        if (choices[i].value == 'a1') {
+          a1score = a1score + 1;
         }
-        if (choices[i].value == 'c2') {
-          c2score = c2score + 1;
+        if (choices[i].value == 'a2') {
+          a2score = a2score + 1;
         }
-        if (choices[i].value == 'c3') {
-          c3score = c3score + 1;
+        if (choices[i].value == 'a3') {
+          a3score = a3score + 1;
         }
-        if (choices[i].value == 'c4') {
-          c4score = c4score + 1;
+        if (choices[i].value == 'a4') {
+          a4score = a4score + 1;
         }
-        if (choices[i].value == 'c5') {
-          c5score = c5score + 1;
+        if (choices[i].value == 'a5') {
+          a5score = a5score + 1;
         }
         // If you add more choices and outcomes, you must add another if statement below.
       }
@@ -39,26 +39,26 @@ function tabulateAnswers() {
     // Find out which choice got the highest score.
     
     // If you add more choices and outcomes, you must add the variable here.
-    var maxscore = Math.max(c1score,c2score,c3score,c4score,c5score);
+    var maxscore = Math.max(a1score,a2score,a3score,a4score,a5score);
     
     // Display answer corresponding to that choice
     var answerbox = document.getElementById('answer');
-    if (c1score == maxscore) { // If user chooses the first choice the most, this outcome will be displayed.
+    if (a1score == maxscore) { 
       answerbox.innerHTML = "You are not showing any signs of ADHD.";
     }
-    if (c2score == maxscore) { // If user chooses the second choice the most, this outcome will be displayed.
+    if (a2score == maxscore) { 
       answerbox.innerHTML = "You are not showing many signs of ADHD.";
     }
-    if (c3score == maxscore) { // If user chooses the third choice the most, this outcome will be displayed.
+    if (a3score == maxscore) { 
       answerbox.innerHTML = "Neurodiversity is a spectrum.";
     }
-    if (c4score == maxscore) { // If user chooses the fourth choice the most, this outcome will be displayed.
+    if (a4score == maxscore) { 
       answerbox.innerHTML = "You show some signs of ADHD";
     }
-    if (c5score == maxscore) { // If user chooses the fourth choice the most, this outcome will be displayed.
+    if (a5score == maxscore) {
       answerbox.innerHTML = "You most likely have ADHD.";
     }
-    // If you add more choices, you must add another response below.
+   
   }
   
   // program the reset button
